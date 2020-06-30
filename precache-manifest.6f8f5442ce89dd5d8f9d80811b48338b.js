5e38ba8a333b9b70d36cc8f0644997db0261359c
JLWolff <joaolwolff@gmail.com>
2020-06-30 14:50:44 +0100

    Updates

---
+++ precache-manifest.6f8f5442ce89dd5d8f9d80811b48338b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1b76f019c1f184663735221b7208e0",
+    "url": "/Titodo/index.html"
+  },
+  {
+    "revision": "066ac571fd091ef60887",
+    "url": "/Titodo/static/css/main.53a0bd39.chunk.css"
+  },
+  {
+    "revision": "095870a947e624a38dee",
+    "url": "/Titodo/static/js/2.6bcfcd02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Titodo/static/js/2.6bcfcd02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066ac571fd091ef60887",
+    "url": "/Titodo/static/js/main.cced89ef.chunk.js"
+  },
+  {
+    "revision": "7617a61ee166b3fe7624",
+    "url": "/Titodo/static/js/runtime-main.824c69bf.js"
+  }
+]);
